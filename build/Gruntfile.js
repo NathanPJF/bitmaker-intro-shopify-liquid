4f7c03ef323ba3f978add06766197462d7c4c7c0
Nathan Ferguson <shout@nathanpjf.com>
2016-01-17 23:45:30 -0500

    re-writeregistered tasks

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -12,10 +12,30 @@ module.exports = function(grunt) {
         cwd: 'build/',
         src: '**',
         dest: '../presentation',
-      }
+      },
+      reveal_js: {
+        expand: true,
+        cwd : 'bower_components/reveal.js/',
+        src: ['**/*.js','!Gruntfile.js'],
+        dest: '../build/assets/scripts/',
+        flatten: true
+      },
+      reveal_css: {
+        expand: true,
+        cwd : 'bower_components/reveal.js/css/',
+        src: ['print/*.css','pdf/*.css','reveal.scss','theme/source/**.*','theme/template/**.*'],
+        dest: '../build/assets/sass/vendor/reveal/'
+      },
+      reveal_font: {
+        expand: true,
+        cwd : 'bower_components/reveal.js/',
+        src: ['font/'],
+        dest: '../build/assets/fonts/',
+        flatten: true
+      },
     },
     clean: {
-      build: {
+      dist: {
         src: [ '../dist/assets/', '!assets/' ],
         options: {
           force: true
@@ -29,6 +49,10 @@ module.exports = function(grunt) {
         },
         files: {                         // Dictionary of files
           '../dist/assets/main.css': 'assets/sass/styles.scss',       // 'destination': 'source'
+          '../dist/styles/reveal.css': 'assets/sass/vendor/reveal/reveal.scss',
+          '../dist/styles/paper.css': 'assets/sass/vendor/reveal/print/paper.css',
+          '../dist/styles/pdf.css': 'assets/sass/vendor/reveal/print/pdf.css',
+          '../dist/styles/black.css': 'assets/sass/vendor/reveal/theme/source/black.scss',
         }
       }
     },
@@ -57,10 +81,17 @@ module.exports = function(grunt) {
     [ 'sass','autoprefixer' ]
   );
   grunt.registerTask('default', ['stylesheets']);
+
+  grunt.registerTask(
+    'copy:assets',
+    'Copy and flatten all vendor assets into dist directory.',
+    [ 'copy:reveal_js','copy:reveal_css', 'copy:reveal_font' ]
+  );
+
   grunt.registerTask(
     'build',
     'Compiles all of the assets and copies the files to the build directory.',
-    [ 'clean:build', 'stylesheets' ]
+    [ 'clean:dist','copy:assets', 'stylesheets' ]
   );
 
 // .module
